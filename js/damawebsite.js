5592282ae6fbba6453bdb1f3e6c3874c5bf0789a
DarioMastro95 <32960577+DarioMastro95@users.noreply.github.com>
2018-01-26 11:13:38 +0100

    Mail def

--- js/damawebsite.js
+++ js/damawebsite.js
@@ -5,75 +5,30 @@ function myMap() {
   var mapOptions = {center: new google.maps.LatLng(51.5, -0.2),zoom: 10};
   var map = new google.maps.Map(mapCanvas, mapOptions);
 }
+function Email() {
+  var email = "info@gruppodama.it";
+  var name = document.getElementById("name").value;
+  var oggetto = document.getElementById("oggetto").value;
+  var messaggio =name+": "+ document.getElementById("message").value;
 
-
-$(function() {
-
-$("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-preventSubmit: true,
-submitError: function($form, event, errors) {
-// additional error messages or events
-},
-submitSuccess: function($form, event) {
-event.preventDefault(); // prevent default submit behaviour
-// get values from FORM
-var name = $("input#name").val();
-var email = $("input#email").val();
-var message = $("textarea#message").val();
-var firstName = name; // For Success/Failure Message
-// Check for white space in name for Success/Fail message
-if (firstName.indexOf(' ') >= 0) {
-firstName = name.split(' ').slice(0, -1).join(' ');
+  if ((email.indexOf("@") == (-1)) || (email == "") || (email == "undefined")) {
+    alert("Inserire un indirizzo email valido.");
+    document.email.focus();
+  }
+  else if ((oggetto == "") || (oggetto == "undefined")) {
+    alert("Inserire un oggetto.");
+    document.oggetto.focus();
+  }
+  else if ((messaggio == "") || (messaggio == "undefined")) {
+    alert("Inserire un messaggio.");
+    document.messaggio.focus();
+  }
+  else {
+    location.href = "mailto:" + email + "?Subject=" + oggetto + "&Body=" + messaggio;
+  }
 }
-$.ajax({
-url: "././mail/contact_me.php",  //script php
-type: "POST",
-data: {
-name: name,
-email: email,
-message: message
-},
-cache: false,
-success: function(response) {
-console.log(response);
-// Success message
-$('#success').html("<div class='alert alert-success'>");
-    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-        .append("</button>");
-    $('#success > .alert-success')
-    .append("<strong>Your message has been sent. </strong>");
-    $('#success > .alert-success')
-    .append('</div>');
 
-//clear all fields
-$('#contactForm').trigger("reset");
-},
-error: function() {
-// Fail message
-$('#success').html("<div class='alert alert-danger'>");
-    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-        .append("</button>");
-    $('#success > .alert-danger').append($("<strong>").text(" We're sorry, there seems to have been a problem with sending your message. We ask you to try again in a few minutes."));
-        $('#success > .alert-danger').append('</div>');
-//clear all fields
-$('#contactForm').trigger("reset");
-},
-});
-},
-filter: function() {
-return $(this).is(":visible");
-},
-});
 
-$("a[data-toggle=\"tab\"]").click(function(e) {
-e.preventDefault();
-$(this).tab("show");
-});
-});
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-$('#success').html('');
-});
 
 
 $('#myCarousel').carousel({
